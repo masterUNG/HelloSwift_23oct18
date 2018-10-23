84f2d42c94e254e9c16f6af4467240ecfa008dce
masterUNG <phrombutr@gmail.com>
2018-10-23 10:17:54 +0700

    Initial Commit

---
+++ HelloSwift_23oct18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift_23oct18
+//
+//  Created by MasterUNG on 23/10/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
